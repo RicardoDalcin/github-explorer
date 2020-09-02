71afbf8644b13027a3bb368d5938d10f6b9856e8
Ricardo <ricardo.dalcinh@gmail.com>
2020-09-02 19:46:06 -0300

    Repository list and details

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
+import GlobalStyles from './styles/global'
 import Routes from './routes'
 
 const App = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <GlobalStyles />
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </>
 )
 
 export default App
